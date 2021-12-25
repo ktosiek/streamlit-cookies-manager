006cb8443161803b32a25950c962c29e20609dc5
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2021-12-25 14:05:52 +0100

    EncryptedCookieManager that encrypts cookies
    This is needed to keep secrets in cookies on Streamlit Cloud.

--- example.py
+++ example.py
@@ -1,10 +1,14 @@
+import os
+
 import streamlit as st
 from streamlit.components.v1 import html
 
-from streamlit_cookies_manager import CookieManager
+from streamlit_cookies_manager import EncryptedCookieManager
 
 # This should be on top of your script
-cookies = CookieManager()
+cookies = EncryptedCookieManager(
+    password=os.environ.get("COOKIES_PASSWORD", "My secret password"),
+)
 if not cookies.ready():
     # Wait for the component to load and send us current cookies.
     st.spinner()
@@ -18,11 +22,22 @@ html("""
     </script>
 """)
 value = st.text_input("New value for a cookie")
-if st.button("Change the cookie"):
-    cookies['a-cookie'] = value
+
+col1, col2 = st.columns(2)
+changed = False
+with col1:
+    if st.button("Change the cookie"):
+        cookies['a-cookie'] = value
+        changed = True
+        assert cookies['a-cookie'] == value, \
+            "CookieManager should return the target value, not the stale one"
+with col2:
+    if st.button("Delete the cookie"):
+        del cookies['a-cookie']
+        changed = True
+
+if changed:
     "I'll save your cookie on next rerun."
-    assert cookies['a-cookie'] == value, \
-        "CookieManager should return the target value, not the stale one"
 
     if st.button("No really, change it now"):
         cookies.save()  # Force saving the cookies now, without a rerun
--- streamlit_cookies_manager/__init__.py
+++ streamlit_cookies_manager/__init__.py
@@ -1,13 +1,123 @@
+import base64
+import os
 from collections import Mapping, MutableMapping
 from datetime import datetime, timedelta
 from pathlib import Path
+from typing import Optional, Tuple
 from urllib.parse import unquote
 
 import streamlit as st
+from cryptography import fernet
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from streamlit.components.v1 import components
 
 build_path = Path(__file__).parent / 'build'
 _component_func = components.declare_component("CookieManager.sync_cookies", path=str(build_path))
+
+
+@st.cache
+def key_from_parameters(salt: bytes, iterations: int, password: str):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=iterations,
+    )
+
+    return base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
+
+
+class EncryptedCookieManager(MutableMapping[str, str]):
+    def __init__(
+            self, *,
+            password: str,
+            path: str = None,
+            key_params_cookie="EncryptedCookieManager.key_params",
+            ignore_broken=True,
+    ):
+        self._cookie_manager = CookieManager(path=path)
+        self._fernet: Optional[Fernet] = None
+        self._key_params_cookie = key_params_cookie
+        self._password = password
+        self._ignore_broken = ignore_broken
+
+    def ready(self):
+        return self._cookie_manager.ready()
+
+    def save(self):
+        return self._cookie_manager.save()
+
+    def _encrypt(self, value):
+        self._setup_fernet()
+        return self._fernet.encrypt(value)
+
+    def _decrypt(self, value):
+        self._setup_fernet()
+        return self._fernet.decrypt(value)
+
+    def _setup_fernet(self):
+        if self._fernet is not None:
+            return
+        key_params = self._get_key_params()
+        if not key_params:
+            key_params = self._initialize_new_key_params()
+        salt, iterations, magic = key_params
+        key = key_from_parameters(
+            salt=salt,
+            iterations=iterations,
+            password=self._password
+        )
+
+        self._fernet = Fernet(key)
+
+    def _get_key_params(self) -> Optional[Tuple[bytes, int, bytes]]:
+        raw_key_params = self._cookie_manager.get(self._key_params_cookie)
+        if not raw_key_params:
+            return
+        try:
+            raw_salt, raw_iterations, raw_magic = raw_key_params.split(':')
+            return base64.b64decode(raw_salt), int(raw_iterations), base64.b64decode(raw_magic)
+        except (ValueError, TypeError):
+            print(f"Failed to parse key parameters from cookie {raw_key_params}")
+            return
+
+    def _initialize_new_key_params(self) -> Tuple[bytes, int, bytes]:
+        salt = os.urandom(16)
+        iterations = 390000
+        magic = os.urandom(16)
+        self._cookie_manager[self._key_params_cookie] = b':'.join([
+            base64.b64encode(salt),
+            str(iterations).encode('ascii'),
+            base64.b64encode(magic)
+        ]).decode('ascii')
+        return salt, iterations, magic
+
+    def __repr__(self):
+        if self.ready():
+            return f'<EncryptedCookieManager: {dict(self)!r}>'
+        return '<EncryptedCookieManager: not ready>'
+
+    def __getitem__(self, k: str) -> str:
+        try:
+            return self._decrypt(self._cookie_manager[k].encode('utf-8')).decode('utf-8')
+        except fernet.InvalidToken:
+            if self._ignore_broken:
+                return
+            raise
+
+    def __iter__(self):
+        return iter(self._cookie_manager)
+
+    def __len__(self):
+        return len(self._cookie_manager)
+
+    def __setitem__(self, key: str, value: str) -> None:
+        self._cookie_manager[key] = self._encrypt(value.encode('utf-8')).decode('utf-8')
+
+    def __delitem__(self, key: str) -> None:
+        del self._cookie_manager[key]
 
 
 class CookieManager(MutableMapping[str, str]):
